5634b1feabfeccfe8bc0c30d7df6d5d325bfe5d3
BlankGodd <blankgodd33@gmail.com>
2020-04-17 01:20:40 +0100

    Finally, I can see my banner

--- Scripts/start.py
+++ Scripts/start.py
@@ -3,12 +3,13 @@
 
 import time, sys
 from pyfiglet import Figlet
+from clint.textui import colored
 
 
-banner = Figlet(font='gothic')
-print(banner.renderText(BlOs-0.01))
+banner = Figlet(font='standard')
+print(banner.renderText("Blank Os - 0.01"))
 
-starting = 'Starting:[          ]'
+starting = 'Starting:[                    ]'
 for i in range(103):
     time.sleep(0.1)
     if i < 101:
@@ -17,7 +18,7 @@ for i in range(103):
         sys.stdout.write('\r' + starting + '100%')
     if i==10 or i==20 or i==30 or i==40 or i==50 or i==60 or i==70 or \
     i==80 or i==90 or i==100:
-        starting = starting.replace(' ',':',1)
+        starting = starting.replace('  ','::',1)
     sys.stdout.flush()
 
 
